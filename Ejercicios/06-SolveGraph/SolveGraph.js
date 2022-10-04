9a30ed91b4894f97bc87f222e87672f7e2b645e1
Alicia Benítez Samaniego <benitez.ali@gmail.com>
2022-10-03 22:55:21 -0300

    resuleto ejecrcicio de los grafos

--- Ejercicios/06-SolveGraph/SolveGraph.js
+++ Ejercicios/06-SolveGraph/SolveGraph.js
@@ -1,6 +1,25 @@
-function SolveGraph (graph, start, end) {
-  // Your code here:
+let graph = {  a: ['c'], b: ['c'], c: ['s', 'r'], d: ['a'], s: ['a', 'c'], r: ['d'],
+  z: ['z'] }
+export default function SolveGraph (graph, start, end ) {
+  let queue = [start]; //Juan Gerardo
+    let visited = new Set();
+    while (queue.length > 0) {
+      let node = queue.shift();
+      if (node === end) {
+        return true;
+      }
+      if (!visited.has(node)) {
+        visited.add(node);
+        queue = queue.concat(graph[node]);
+      }
+    }
+    return false;
+  }
 
-}
+module.exports = SolveGraph;
 
-module.exports = SolveGraph
+
+console.log(SolveGraph(graph, 'a', 'r'))//true
+console.log(SolveGraph(graph, 'a', 'd'))//true
+console.log(SolveGraph(graph, 's', 'b'))//false
+
